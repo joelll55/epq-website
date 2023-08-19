662acbdadd495553f13f08efe00695979da03240
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-19 19:11:17 +0100

    fix lazy image error

--- src/components/Features/Main.vue
+++ src/components/Features/Main.vue
@@ -1,6 +1,6 @@
 <template>
 	<n-layout has-sider class="main-layout">
-		<n-layout-sider> <n-image :src="props.image" lazy width="250" /> </n-layout-sider>
+		<n-layout-sider> <n-image :src="props.image" width="250" /> </n-layout-sider>
 		<n-layout-content class="main-layout-content"
 			><h2>{{ props.title }}</h2>
 			<p style="padding-bottom: 15px">
