6614fe51c891239065f285cc2ca2c9103ac43599
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-19 19:11:31 +0100

    leaderboard layout

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,8 +1,41 @@
 <template>
-    <main>
-        <h1>Leaderboard</h1>
-        <p>I am the best</p>
-    </main>
+	<div>
+		<n-tabs type="segment">
+			<n-tab-pane name="beginner" tab="Beginner"><n-data-table :columns="columns" :data="beginnerData"></n-data-table></n-tab-pane>
+			<n-tab-pane name="intermediate" tab="Intermediate"><n-data-table :columns="columns" :data="intermediateData"></n-data-table></n-tab-pane>
+			<n-tab-pane name="advanced" tab="Advanced"><n-data-table :columns="columns" :data="advancedData"></n-data-table></n-tab-pane>
+		</n-tabs>
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { NDataTable, NTabs, NTabPane } from 'naive-ui'
+
+const beginnerData = [{ rank: 1, name: 'test', score: 10000 }]
+const intermediateData = [
+	{ rank: 1, name: 'test', score: 1000 },
+	{ rank: 2, name: 'test1', score: 500 }
+]
+const advancedData = [{ rank: 1, name: 'test', score: 100 }]
+
+onMounted(() => {
+	console.log('Load Leaderboard')
+	// TODO: load leaderboard from server
+})
+
+const columns = [
+	{
+		title: 'Rank',
+		key: 'rank'
+	},
+	{
+		title: 'Name',
+		key: 'name'
+	},
+	{
+		title: 'Score',
+		key: 'score'
+	}
+]
+</script>
