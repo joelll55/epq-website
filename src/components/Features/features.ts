f0c4431ce1bddbd5f5fed4b67d6512e14efd39d7
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-29 18:46:32 +0100

    fix gifs on home page

--- src/components/Features/features.ts
+++ src/components/Features/features.ts
@@ -2,18 +2,18 @@ export const homePageFeatures = [
 	{
 		title: 'Easy to use',
 		description: 'To get started simply register an account or login if you already have one, navigate to the "Play" page and select your skill level. Then you can start playing instantly!',
-		image: '/public/easytouse.gif'
+		image: '/easytouse.gif'
 	},
 	{
 		title: 'Variety of difficulties',
 		description:
 			'Before you start playing you can select your skill level. This will modify how your unique questions are generated. There are three difficulties to choose from: beginner, intermediate and advanced.',
-		image: '/public/difficulties.gif'
+		image: '/difficulties.gif'
 	},
 	{
 		title: 'Competitive Leaderboards',
 		description:
 			"When you play you will be able to see your highest score and how it ranks against other player's high scores. The competition to reach and stay on the top will motivate you to keep playing and improving your score!",
-		image: '/public/leaderboard.gif'
+		image: '/leaderboard.gif'
 	}
 ]
