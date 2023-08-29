7e1bc246b5d9e70882568e71ab980ef739bd1778
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-29 22:18:11 +0100

    actually fix gifs

--- src/components/Features/Main.vue
+++ src/components/Features/Main.vue
@@ -1,6 +1,6 @@
 <template>
 	<n-layout has-sider class="main-layout">
-		<n-layout-sider class="bg-remove" width="300"> <n-image :src="props.image" width="300" style="margin-block: 10%" /> </n-layout-sider>
+		<n-layout-sider class="bg-remove" width="300"> <n-image :src="loadedImage" width="300" style="margin-block: 10%" /> </n-layout-sider>
 		<n-layout-content class="main-layout-content"
 			><h2>{{ props.title }}</h2>
 			<p style="padding-bottom: 15px">
@@ -14,6 +14,8 @@
 import { NLayout, NLayoutSider, NLayoutContent, NImage } from 'naive-ui'
 
 const props = defineProps(['title', 'description', 'image'])
+
+const loadedImage = new URL(props.image, import.meta.url).href
 </script>
 
 <style scoped>
